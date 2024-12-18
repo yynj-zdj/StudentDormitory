478f881317e1393f9e5f8c1df621f726fe9e5cb2
yynj-zdj <1930965623@qq.com>
2024-12-18 18:34:16 +0800

    项目

---
+++ src/main/java/cn/lanqiao/studentdormitory/StudentDormitoryApplication.java
@@ -0,0 +1,14 @@
+package cn.lanqiao.studentdormitory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class StudentDormitoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentDormitoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example1/studentdormitory/StudentDormitoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example1.studentdormitory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDormitoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
